027da97dd14f0ec3ba2d301abfbb289f0d3be5f0
ashotisraelyan <ashotisraelyan@gmail.com>
2024-05-22 17:19:59 +0400

    feat: swipe to delete and segmented control icons added

--- app/(tabs)/calls/index.tsx
+++ app/(tabs)/calls/index.tsx
@@ -1,12 +1,4 @@
-import {
-	FlatList,
-	Image,
-	ScrollView,
-	StyleSheet,
-	Text,
-	TouchableOpacity,
-	View,
-} from 'react-native';
+import { Image, ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { Stack } from 'expo-router';
 import { useState } from 'react';
 import Colors from '@/constants/Colors';
@@ -15,17 +7,37 @@ import { defaultStyles } from '@/constants/Styles';
 import { Ionicons } from '@expo/vector-icons';
 import { format } from 'date-fns';
 import SegmentedControl from '@/components/SegmentedControl';
-import Animated, { CurvedTransition, FadeInUp, FadeOutUp } from 'react-native-reanimated';
+import Animated, {
+	CurvedTransition,
+	FadeInUp,
+	FadeOutUp,
+	useAnimatedStyle,
+	useSharedValue,
+	withTiming,
+} from 'react-native-reanimated';
+import SwipeableRow from '@/components/SwipeableRow';
+import * as Haptics from 'expo-haptics';
 
 const transition = CurvedTransition.delay(100);
+
+const AnimatedTouchableOpacity = Animated.createAnimatedComponent(TouchableOpacity);
 
 const Page = () => {
 	const [selectedOption, setSelectedOption] = useState('All');
 	const [isEditing, setIsEditing] = useState(false);
 	const [items, setItems] = useState(calls);
 
+	const editing = useSharedValue(-30);
+
+	const removeCall = (toDelete: any) => {
+		Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light);
+		setItems(items.filter((item) => item.id !== toDelete.id));
+	};
+
 	const onEdit = () => {
-		setIsEditing((prev) => !prev);
+		const editingNew = !isEditing;
+		editing.value = editingNew ? 0 : -30;
+		setIsEditing(editingNew);
 	};
 
 	const onSegmentChange = (option: string) => {
@@ -36,6 +48,10 @@ const Page = () => {
 			setItems(calls.filter((call) => call.missed));
 		}
 	};
+
+	const animatedPosition = useAnimatedStyle(() => ({
+		transform: [{ translateX: withTiming(editing.value) }],
+	}));
 
 	return (
 		<View style={{ flex: 1, backgroundColor: Colors.background }}>
@@ -71,38 +87,57 @@ const Page = () => {
 						ItemSeparatorComponent={() => <View style={defaultStyles.separator} />}
 						itemLayoutAnimation={transition}
 						renderItem={({ item, index }) => (
-							<Animated.View entering={FadeInUp.delay(index * 20)} exiting={FadeOutUp}>
-								<View style={[defaultStyles.item]}>
-									<Image source={{ uri: item.img }} style={styles.avatar} />
-
-									<View style={{ flex: 1, gap: 2 }}>
-										<Text style={{ fontSize: 18, color: item.missed ? Colors.red : '#000' }}>
-											{item.name}
-										</Text>
-
-										<View style={{ flexDirection: 'row', gap: 4 }}>
-											<Ionicons
-												name={item.video ? 'videocam' : 'call'}
-												size={16}
-												color={Colors.gray}
-											/>
-											<Text style={{ color: Colors.gray, flex: 1 }}>
-												{item.incoming ? 'Incoming' : 'Outgoing'}
-											</Text>
-										</View>
-									</View>
-									<View
-										style={{
-											flexDirection: 'row',
-											gap: 6,
-											alignItems: 'center',
-										}}
+							<SwipeableRow onDelete={() => removeCall(item)}>
+								<Animated.View
+									style={{ flexDirection: 'row', alignItems: 'center' }}
+									entering={FadeInUp.delay(index * 20)}
+									exiting={FadeOutUp}
+								>
+									<AnimatedTouchableOpacity
+										onPress={() => removeCall(item)}
+										style={[animatedPosition, { paddingLeft: 8 }]}
 									>
-										<Text style={{ color: Colors.gray }}>{format(item.date, 'MM.dd.yy')}</Text>
-										<Ionicons name="information-circle-outline" size={24} color={Colors.primary} />
-									</View>
-								</View>
-							</Animated.View>
+										<Ionicons name="remove-circle" size={24} color={Colors.red} />
+									</AnimatedTouchableOpacity>
+
+									<Animated.View
+										style={[defaultStyles.item, animatedPosition, { paddingLeft: 20 }]}
+									>
+										<Image source={{ uri: item.img }} style={styles.avatar} />
+
+										<View style={{ flex: 1, gap: 2 }}>
+											<Text style={{ fontSize: 18, color: item.missed ? Colors.red : '#000' }}>
+												{item.name}
+											</Text>
+
+											<View style={{ flexDirection: 'row', gap: 4 }}>
+												<Ionicons
+													name={item.video ? 'videocam' : 'call'}
+													size={16}
+													color={Colors.gray}
+												/>
+												<Text style={{ color: Colors.gray, flex: 1 }}>
+													{item.incoming ? 'Incoming' : 'Outgoing'}
+												</Text>
+											</View>
+										</View>
+										<View
+											style={{
+												flexDirection: 'row',
+												gap: 6,
+												alignItems: 'center',
+											}}
+										>
+											<Text style={{ color: Colors.gray }}>{format(item.date, 'MM.dd.yy')}</Text>
+											<Ionicons
+												name="information-circle-outline"
+												size={24}
+												color={Colors.primary}
+											/>
+										</View>
+									</Animated.View>
+								</Animated.View>
+							</SwipeableRow>
 						)}
 					/>
 				</Animated.View>
