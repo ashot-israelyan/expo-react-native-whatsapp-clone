d7fd354683ef10ad7a55be10691277c6e9c3439e
ashotisraelyan <ashotisraelyan@gmail.com>
2024-05-23 22:36:08 +0400

    feat: single chat screen with reply message implemented

--- app/(tabs)/chats/_layout.tsx
+++ app/(tabs)/chats/_layout.tsx
@@ -1,6 +1,6 @@
 import { Link, Stack } from 'expo-router';
 import Colors from '@/constants/Colors';
-import { TouchableOpacity, View } from 'react-native';
+import { Image, Text, TouchableOpacity, View } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 
 const Layout = () => {
@@ -42,7 +42,45 @@ const Layout = () => {
 					),
 				}}
 			/>
-			<Stack.Screen name="[id]" />
+			<Stack.Screen
+				name="[id]"
+				options={{
+					title: '',
+					headerBackTitleVisible: false,
+					headerTitle: () => (
+						<View
+							style={{
+								flexDirection: 'row',
+								width: 220,
+								alignItems: 'center',
+								gap: 10,
+								paddingBottom: 4,
+							}}
+						>
+							<Image
+								source={{
+									uri: 'https://pbs.twimg.com/profile_images/1564203599747600385/f6Lvcpcu_400x400.jpg',
+								}}
+								style={{ width: 40, height: 40, borderRadius: 50 }}
+							/>
+							<Text style={{ fontSize: 16, fontWeight: '500' }}>Simon Grimm</Text>
+						</View>
+					),
+					headerRight: () => (
+						<View style={{ flexDirection: 'row', gap: 30 }}>
+							<TouchableOpacity>
+								<Ionicons name="videocam-outline" color={Colors.primary} size={30} />
+							</TouchableOpacity>
+							<TouchableOpacity>
+								<Ionicons name="call-outline" color={Colors.primary} size={30} />
+							</TouchableOpacity>
+						</View>
+					),
+					headerStyle: {
+						backgroundColor: Colors.background,
+					},
+				}}
+			/>
 		</Stack>
 	);
 };
